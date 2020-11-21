e0936125616dc8d459ae6d70164661215fa4190b
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-21 23:08:19 +0100

    Created listener object and created new activity for pens category

--- app/src/main/java/com/linuxkyrios/selcorproject/MainActivity.java
+++ app/src/main/java/com/linuxkyrios/selcorproject/MainActivity.java
@@ -1,14 +1,34 @@
 package com.linuxkyrios.selcorproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.content.Intent;
+import android.widget.ListView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creating listener object
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView, //implementing onItemClick method
+                                    View itemView,
+                                    int position,
+                                    long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(MainActivity.this, PenCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        //Adding listener object to list view
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
---
+++ app/src/main/java/com/linuxkyrios/selcorproject/PenCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.linuxkyrios.selcorproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PenCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pen_category);
+    }
+}
