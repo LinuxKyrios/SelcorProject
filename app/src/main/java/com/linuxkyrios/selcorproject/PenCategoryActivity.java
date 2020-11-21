8ea4ae9739aa7c0b5b3cef2190330e0cd01c98a2
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-21 23:56:25 +0100

    Created code for uploading data from pens array defined in Pen.java also created layout for Pen Category Activity.

--- app/src/main/java/com/linuxkyrios/selcorproject/PenCategoryActivity.java
+++ app/src/main/java/com/linuxkyrios/selcorproject/PenCategoryActivity.java
@@ -1,14 +1,27 @@
 package com.linuxkyrios.selcorproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+import android.widget.AdapterView;
 
-public class PenCategoryActivity extends AppCompatActivity {
+public class PenCategoryActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pen_category);
+
+        //This part of code fulfills list view with data from pens array
+        ArrayAdapter<Pen> listAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Pen.pens);
+        ListView listPens = (ListView) findViewById(R.id.list_pens);
+        listPens.setAdapter(listAdapter);
+
     }
 }
